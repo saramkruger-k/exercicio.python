f61292eb9b776a79cbe0014de34e49cd2015286a
saramkruger-k <sara_m_kruger@estudante.sesisenai.org.br>
2026-09-17 09:24:20 -0300

    exercicio8

---
+++ parte2-condicionais/exercicio7.py
@@ -0,0 +1,8 @@
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+if n1 > n2:
+    print(f"O número {n1} é maior")
+elif n2 > n1:
+    print(f"O número {n2} é maior")
+else:
+    print("Os números são iguais")
